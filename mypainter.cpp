fb3deac76da1f049b846a6f9ff20d317d4ebca1a
dhlin <542072149@qq.com>
2020-07-13 19:36:42 +0800

    init

--- mypainter.cpp
+++ mypainter.cpp
@@ -17,33 +17,30 @@ MyPainter::MyPainter(QWidget *parent)
     connect(timer, &QTimer::timeout, [=]() {
         QPainterPath path;
         lineList.clear();
-        if (pointList.size() > 0) {
-            path.moveTo(pointList[0].p);
-            pointList[0].w = qMin(pointList[0].w + speed, maxWidth);
+        for (int i = 0; i < pointList.size(); i ++) {
+            pointList[i].w = qMin(pointList[i].w + speed, maxWidth);
         }
-        for (int i = 1; i < pointList.size(); i ++) {
-            PointData d1 = pointList[i - 1];
-            PointData d2 = pointList[i];
-            qreal lw = d1.w;
-            qreal w = qMin(d2.w + speed, maxWidth);
-            pointList[i].w = w;
-//            if (i == pointList.size() - 1) {
-//                qDebug() << "i" << i << "d1.w" << d1.w << "w" << w << "lw" << lw << "delta" << w -lw;
+//        if (pointList.size() > 0) {
+//            path.moveTo(pointList[0].p);
+//            pointList[0].w = qMin(pointList[0].w + speed, maxWidth);
+//        }
+//        for (int i = 1; i < pointList.size(); i ++) {
+//            PointData d1 = pointList[i - 1];
+//            PointData d2 = pointList[i];
+//            qreal lw = d1.w;
+//            qreal w = qMin(d2.w + speed, maxWidth);
+//            pointList[i].w = w;
+//            if (lw - w <= 0.0000001) {
+//                path.lineTo(d2.p);
+//            } else {
+//                lineInfo i;
+//                i.setPath(path);
+//                i.setW(lw);
+//                lineList << i;
+//                path.clear();
+//                path.moveTo(d1.p);
 //            }
-            //qDebug() << "i" << i << "d2.w" << d2.w << qAbs(d2.w - d1.w);
-            if (lw - w <= 0.0000001) {
-                //qDebug() << "line to";
-                path.lineTo(d2.p);
-            } else {
-                lineInfo i;
-                i.setPath(path);
-                i.setW(lw);
-                lineList << i;
-              //  qDebug() << "new line";
-                path.clear();
-                path.moveTo(d1.p);
-            }
-        }
+//        }
         update();
     });
 }
@@ -52,37 +49,37 @@ void MyPainter::paintEvent(QPaintEvent *)
 {
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing, true);
-    //QPainterPath path;
+    QPainterPath path;
     if (pointList.size() <= 0) {
         return;
     }
-//    path.moveTo(pointList[0].p);
-//    for (int i = 1; i < pointList.size(); i ++) {
-//        PointData d1 = pointList[i - 1];
-//        PointData d2 = pointList[i];
-//        pen.setWidthF(d1.w);
-//        painter.setPen(pen);
-//        if (d1.w - d2.w <= 0.0000001) {
-//            path.lineTo(d2.p);
-//        } else {
-//            painter.strokePath(path, pen);
-//            path.clear();
-//            path.moveTo(d1.p);
-//        }
-//        if (d1.w < maxWidth)
-//            painter.drawLine(d1.p, d2.p);
-//    }
-//    painter.strokePath(path, pen);
-    qDebug() << "line list size" << lineList.size();
-    for (int i = 0; i < lineList.size(); i ++) {
-        lineInfo line = lineList.at(i);
-        QPainterPath path = line.getPath();
-
-        qreal w = line.getW();
-        //qDebug() << "w" << w;
-        pen.setWidthF(w);
-        painter.strokePath(path, pen);
+    path.moveTo(pointList[0].p);
+    for (int i = 1; i < pointList.size(); i ++) {
+        PointData d1 = pointList[i - 1];
+        PointData d2 = pointList[i];
+        pen.setWidthF(d1.w);
+        painter.setPen(pen);
+        if (d1.w - d2.w <= 0.0000001) {
+            path.lineTo(d2.p);
+        } else {
+            painter.strokePath(path, pen);
+            path.clear();
+            path.moveTo(d1.p);
+        }
+        if (d1.w < maxWidth)
+            painter.drawLine(d1.p, d2.p);
     }
+    painter.strokePath(path, pen);
+//    qDebug() << "line list size" << lineList.size();
+//    for (int i = 0; i < lineList.size(); i ++) {
+//        lineInfo line = lineList.at(i);
+//        QPainterPath path = line.getPath();
+
+//        qreal w = line.getW();
+//        //qDebug() << "w" << w;
+//        pen.setWidthF(w);
+//        painter.strokePath(path, pen);
+//    }
 }
 
 void MyPainter::mousePressEvent(QMouseEvent *event)
